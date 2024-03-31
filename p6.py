e1e3538a4a2914fb0eaa19fb830b4b00b9793673
shanmukh31 <156568261+shanmukh31@users.noreply.github.com>
2024-03-31 15:49:33 +0530

    Add files via upload
    Image to String QR Codes: This prominently highlights the unique feature of encoding an image as a string before creating QR codes.
    
    Generator: It emphasizes the primary function of generating QR codes.
    
    (with Visual Display): This adds a crucial detail that the program not only creates QR codes but also visually displays the original image and the generated QR code.

---
+++ p6.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pyqrcode
+s = cv2.imread("C:\\Users\\Usha\\OneDrive\\Documents\\Desktop\\output.png")
+img_vec = np.array2string(s)
+url = pyqrcode.create(img_vec)
+plt.imshow(img_vec)
+plt.show()
+url.svg("myqr.svg", scale=2)
+url.png("qrcode.png", scale=3)
+qr_code = cv2.imread("qrcode.png")
+cv2.imshow("QR Code", qr_code)
