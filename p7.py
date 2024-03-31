73f5a5cf351a57c7de045afce307618f4343b13b
shanmukh31 <156568261+shanmukh31@users.noreply.github.com>
2024-03-31 15:51:26 +0530

    Add files via upload
    Real-Time: This emphasizes that the program processes video input from a webcam (source 0 in VideoStream(src=0)) continuously, making it suitable for real-time QR code detection.
    
    QR Code Scanner: This accurately conveys the program's main functionality of identifying and decoding QR codes present in the video frames.
    
    Visual Detection: This indicates that the program draws a green rectangle around detected QR codes for visual confirmation.
    
    Output Actions: This highlights that the program not only decodes the QR code data but also prints "open" or "stop" messages based on the decoded content, suggesting potential automation based on the QR code information.

---
+++ p7.py
@@ -0,0 +1,32 @@
+from time import sleep
+from imutils.video import VideoStream
+from pyzbar import pyzbar
+import imutils
+import time
+import cv2
+import os,sys
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+while True:
+    frame = vs.read()
+    frame = imutils.resize(frame,width=400)
+    QRcodes = pyzbar.decode(frame)
+    for QRcodes in QRcodes:
+        (x,y,w,h) = QRcodes.rect
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+        QRcodeData = QRcodes.data.decode("utf-8")
+        QRcodeType = QRcodes.type
+        text="{}".format(QRcodeData)
+        cv2.putText(frame,text,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2)
+        if QRcodeData=='open':
+            print("open")
+        else:
+            print("stop")
+    cv2.imshow("QRcode Scanner",frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+cv2.destroyAllWindows()
+vs.stop()
+    
