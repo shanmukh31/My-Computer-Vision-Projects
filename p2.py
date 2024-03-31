81e01f26a0ca1e3d40a8d4d81fabe3648ae12189
shanmukh31 <156568261+shanmukh31@users.noreply.github.com>
2024-03-31 15:37:33 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy
+video = cv2.VideoCapture("C:\\Users\\Usha\\OneDrive\\Documents\\Desktop\\PYTHON_WORKSHOP_Bore\\goodfellas_henry.mp4")
+while True:
+    ret, img = video.read()
+    cv2.imshow('original color video',img)
+    frame=cv2.resize(img,(350,350))
+    cv2.imshow('resized frame',img)
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('gray scale converted!',gray)
+    ret,bw=cv2.threshold(gray,100,255,cv2.THRESH_BINARY)
+    cv2.imshow('black and white',bw)
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
